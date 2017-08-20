92f75c2ce47fc4013fff4de23903b150e38683e7
Andrew <apc53c@gmail.com>
2017-08-20 19:57:33 +0100

    Added response to GET.

--- manage.py
+++ manage.py
@@ -7,5 +7,12 @@ app = Flask(__name__)
 def static_page():
     return render_template('index.html')
 
+@app.route('/', methods = ['GET'])
+def api_respond():
+    data = {'message':"This is a reply from Flask"}
+    js = json.dumps(data)
+    resp = Response(js, status=200, mimetype='application/json')
+    return resp
+
 if __name__ == "__main__":
     app.run(debug=True, use_reloader=True)
